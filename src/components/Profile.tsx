8c656b8f367a2df6b9bcdda06beaa52ce976e114
mahmutcant <mahcan012@gmail.com>
2023-08-20 12:58:36 +0300

    profile properties made changeable

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,9 +3,27 @@ import { AppState } from "../store/types"
 import Sidebar from "./Sidebar";
 import { useNavigate } from "react-router-dom";
 import './Profile.css'
+import { SubmitHandler, useForm } from "react-hook-form";
 function Profile() {
+    const { register: saveChanges, handleSubmit: handleSubmitRegister, formState: { errors: registerError } } = useForm<User>();
     const user = useSelector((state: AppState) => state.user);
     const navigate = useNavigate()
+    const saveChangesSubmit: SubmitHandler<User> = (data) => {
+        //registerService(data)
+        if(data.name === ""){
+            data.name = user!.name
+        }
+        if(data.surname === ""){
+            data.surname = user!.surname
+        }
+        if(data.username === ""){
+            data.username = user!.username
+        }
+        if(data.email === ""){
+            data.email = user!.email
+        }
+        console.log(data)
+    }
     return (
         <div className="App" style={{ "background": "linear-gradiend" }}>
             <Sidebar />
@@ -13,24 +31,25 @@ function Profile() {
                 <div className="card-content">
                     <div className="card-item">
                         <div className="card-key">Ad:</div>
-                        <div className="card-value">{user?.name}</div>
-                        <i className="profile_icon fa-solid fa-pencil"></i>
+                        <input className="card-value" defaultValue={user?.name}
+                            {...saveChanges('name')} />
                     </div>
                     <div className="card-item">
                         <div className="card-key">Soyad:</div>
-                        <div className="card-value">{user?.surname}</div>
-                        <i className="profile_icon fa-solid fa-pencil"></i>
+                        <input className="card-value" defaultValue={user?.surname}
+                            {...saveChanges('surname')} />
                     </div>
                     <div className="card-item">
                         <div className="card-key">Kullanıcı Adı:</div>
-                        <div className="card-value">{user?.username}</div>
-                        <i className="profile_icon fa-solid fa-pencil"></i>
+                        <input className="card-value" defaultValue={user?.username}
+                            {...saveChanges('username')} />
                     </div>
                     <div className="card-item">
                         <div className="card-key">E-mail:</div>
-                        <div className="card-value">{user?.email}</div>
-                        <i className="profile_icon fa-solid fa-pencil"></i>
+                        <input className="card-value" defaultValue={user?.email}
+                            {...saveChanges('email')} />
                     </div>
+                    <button className="btn btn-primary save_button" onClick={() => handleSubmitRegister(saveChangesSubmit)()}>Değişiklikleri Kaydet</button>
                 </div>
             </div>
         </div>
