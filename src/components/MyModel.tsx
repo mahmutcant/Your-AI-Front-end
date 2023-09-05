011c4769e48aec0a3512ed8fbcb1aa1afc0077f9
mahmutcant <mahcan012@gmail.com>
2023-09-05 18:38:35 +0300

    feat: model accuracy shown on screen

--- src/components/MyModel.tsx
+++ src/components/MyModel.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import { getMyModels } from "../services/model-service";
-
+import './MyModel.css'
 function MyModel() {
     const [myModels,setMyModels] = useState<ModelNames[] | null>([]);
     useEffect(() => {
@@ -11,17 +11,26 @@ function MyModel() {
         }
         getModelInfo()
     }, [])
+    const getAModel = (id:number) => {
+        console.log(id)
+    }
     return (
         <div>
             <Sidebar />
             <div className="card file-upload-card">
                 <h1 className="text-black">Kayıtlı Modeller</h1>
-                <ul>
+                <div className="scrollable-content">
+                <ul className="noselect my-list">
                     {myModels && Object.values(myModels).map((key,index) => (
                         
-                        <li>{key.modelSpecialName}</li>
+                        <li onClick={() => {getAModel(key.id)}} key={index}>
+                            {key.modelSpecialName}
+                            <br/>
+                            Başarı Yüzdesi :  % {(key.accuracyValue * 100).toFixed(1)}
+                        </li>
                     ))}
                 </ul>
+                </div>
             </div>
         </div>
     )
--- src/models/modelname-model.ts
+++ src/models/modelname-model.ts
@@ -1,4 +1,7 @@
 interface ModelNames{
     id: number;
     modelSpecialName: string;
+    csvData: object;
+    path: string;
+    accuracyValue: number
 }
